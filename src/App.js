22fa3da3424b24142f4b138a4d42e0c16734f820
Ian Homer <ihomer@worth.systems>
2019-09-30 20:43:23 +0100

    React and mermaid

--- src/App.js
+++ src/App.js
@@ -5,18 +5,34 @@ import './App.css';
 class App extends Component {
   componentDidMount() {
     const output = document.getElementById("output");
-    mermaid.initialize({ startOnLoad: true});
-
-    var graph = `
-  graph LR
-  A --- B
-  B-->C[fa:fa-ban forbidden]
-  B-->D(fa:fa-spinner);
-`;
-
-    mermaid.render('theGraph', graph, (html) => {
-      output.innerHTML = html;
+    mermaid.initialize({
+      startOnLoad: true
     });
+
+    const graph1 = `
+graph LR
+  A[square] --- B(Round)
+  B-->C{Decision}
+  B-->|Label| D(Result one)
+`
+
+    const graph2 = `  
+sequenceDiagram
+    participant Alice
+    participant Bob
+    Alice->>John: Hello John, how are you?
+    loop Healthcheck
+        John->>John: Fight against hypochondria
+    end
+    Note right of John: Rational thoughts <br/>prevail!
+    John-->>Alice: Great!
+    John->>Bob: How about you?
+    Bob-->>John: Jolly good!  
+`
+
+    mermaid.render('graph', graph1, (html) => {
+      document.getElementById(`output`).innerHTML = html;
+    })
   }
 
   render() {
